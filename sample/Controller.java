506e50836c0ed132d4a72882ea5eff29d7ce86d6
Y Tran <ytran@trentu.ca>
2021-03-31 21:47:03 -0400

    lab 6 & 7 database

--- sample/Controller.java
+++ sample/Controller.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.fxml.FXML;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -9,22 +10,34 @@ public class Controller {
     private TextField name;
     @FXML
     private TextField ID;
+    @FXML
+    private Button LogIn;
+
 
     public void onButtonClicked(){
+        Helper.CreateTable(Helper.ConnectToDatabase());
+        boolean valid = true;
         String userName =name.getText(); //get Student name
         //show this message if the user enters invalid name
         if(Helper.validateName(userName)==false){
             System.out.println("ERROR: Invalid Name");
+            valid = false;
         }else{  //otherwise, show this
-            System.out.println("Student Name is Valid.");
+            System.out.println("Student Name is valid.");
         }
         //get Student ID
         String number = ID.getText();
         //show error message if the user enters invalid ID number
         if(Helper.validateID(number)==false){
             System.out.println("ERROR: Invalid Student ID");
+            valid = false;
         }else{      //otherwise, print this
             System.out.println("Student ID is valid");
+        }
+        if(valid ==true){
+           LogIn.setDisable(true);
+            Helper.InsertRecord(Helper.ConnectToDatabase(), userName, number);
+            System.out.println("Your input was saved into the database.");
         }
 
     }
--- sample/Helper.java
+++ sample/Helper.java
@@ -1,6 +1,57 @@
 package sample;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Helper {
+    //create the database file
+    public static Connection ConnectToDatabase(){
+        try{
+
+            Connection conn =
+                    DriverManager.getConnection("JDBC:sqlite:testjava.db");
+            return conn;
+
+        }catch(SQLException e){
+
+            System.out.println("Error: " + e.getMessage());
+            return null;
+
+        }
+    }
+    //create the table
+    public static boolean CreateTable(Connection conn){
+        try{
+
+            Statement query = conn.createStatement();
+
+            query.execute("CREATE TABLE IF NOT EXISTS users (studentName TEXT, ID TEXT)");
+            return true;
+
+        }catch(SQLException e){
+            System.out.println("Error: " +  e.getMessage());
+            return false;
+        }
+    }
+    //save valid user input into the database
+    public static boolean InsertRecord(Connection conn, String userName, String ID){
+
+        try{
+
+            Statement query = conn.createStatement();
+            query.execute("INSERT INTO users VALUES('"+userName+"','"+ID+"')");
+            return true;
+
+        }catch(SQLException e){
+
+            System.out.println("Error: " + e.getMessage());
+            return false;
+        }
+    }
+
+
     //validate the student name, only allows a-z upper and lower case
     //and space between
     public static boolean validateName(String userName){
